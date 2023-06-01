1aee7534cdd122e6a6333bd926173f4e7ee36b1d
Grainne Donegan <grainnejdonegan@gmail.com>
2023-06-01 20:03:42 +0000

    integrate allauth into project

--- the_roastery/settings.py
+++ the_roastery/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'home',
 ]
 
@@ -72,6 +76,13 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
 
 WSGI_APPLICATION = 'the_roastery.wsgi.application'
 
