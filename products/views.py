d0bbf40cf41ad53e69e3cdf75f8ce18036d0c7e4
Grainne Donegan <grainnejdonegan@gmail.com>
2023-06-03 22:07:02 +0000

    update filter queries in products.html

--- products/views.py
+++ products/views.py
@@ -1,13 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from .models import *
+from django.db.models import Q
 
 def all_products(request):
-    """ A view to show all products, including sorting and search queries """
-
+    
     products = Product.objects.all()
+    sort = None
+    direction = None
+    search = None
+
+    print(request.GET)
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+        if 'search_bar' in request.GET:
+            search = request.GET['search_bar']
+            if 'roast' in search:
+                search = search.replace('roast', '')
+            if not search:
+                #error message
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=search) | Q(review__icontains=search) | Q(roast__icontains=search)
+            products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
+        'search_term': search,
+        'filter_element': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
