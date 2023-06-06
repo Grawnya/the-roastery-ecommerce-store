9a405880799f7a82a099e86546ff696b906739dd
Grainne Donegan <grainnejdonegan@gmail.com>
2023-06-06 20:52:03 +0000

    add function to increment values

--- products/static/products/js/quantity_buttons.js
+++ products/static/products/js/quantity_buttons.js
@@ -1,20 +1,32 @@
-function disableButtons(ProductID) {
-    var specificProductQuantity = parseInt($(`.quantity_of_${ProductID}`).val());
+function disableButtons(productID) {
+    var specificProductQuantity = parseInt($(`.quantity_of_${productID}`).val());
     var disablePlusButton = specificProductQuantity > 48;
     var disableMinusButton = specificProductQuantity < 2;
 
-    $(`#minus_button_of_${ProductID}`).prop('disabled', disableMinusButton);
-    $(`#plus_button_of_${ProductID}`).prop('disabled', disablePlusButton);
+    $(`.minus_button_of_${productID}`).prop('disabled', disableMinusButton);
+    $(`.plus_button_of_${productID}`).prop('disabled', disablePlusButton);
     
 }
 
 var quantityOfProduct = $('.quantity_value');
 for(var j = 0; j < quantityOfProduct.length; j++) {
-    var ProductID = $(quantityOfProduct[i]).data('product_id');
-    disableButtons(ProductID);
+    var productID = $(quantityOfProduct[i]).data('product_id');
+    disableButtons(productID);
 }
 
 $('.quantity_value').change(function() {
-    var ProductID = $(this).data('product_id');
-    disableButtons(ProductID);
+    var productID = $(this).data('product_id');
+    disableButtons(productID);
+});
+
+$('.add_more').click(function(event) {
+    event.preventDefault();
+    var productID = $(this).data('product_id');
+    var productInput = $(this).closest('.input-group').find('.quantity_value')[0];
+    var specificQuantityInput = $(`.input-group-single-${productID} input[name='order_quantity']`);
+    var specificProductQuantity = parseInt($(productInput).val());
+    
+    var newValue = specificProductQuantity + 1;
+    $(specificQuantityInput).val(newValue);
+    disableButtons(productID);
 });
