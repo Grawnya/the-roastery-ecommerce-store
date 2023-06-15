2c41cad52e3d9e04bb65af4a75a76231b7f8c841
Grainne Donegan <grainnejdonegan@gmail.com>
2023-06-15 19:43:00 +0000

    add go to checkout button

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -23,3 +23,42 @@ def add_item(request, item_id):
 
     request.session['shopping_bag'] = shopping_bag
     return redirect('products')
+
+def update_bag(request, item_id):
+    """Update the quantity of the product to a new amount"""
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    bag = request.session.get('bag', {})
+
+    if size:
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
+            messages.success(request,
+                             (f'Updated size {size.upper()} '
+                              f'{product.name} quantity to '
+                              f'{bag[item_id]["items_by_size"][size]}'))
+        else:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+            messages.success(request,
+                             (f'Removed size {size.upper()} '
+                              f'{product.name} from your bag'))
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+            messages.success(request,
+                             (f'Updated {product.name} '
+                              f'quantity to {bag[item_id]}'))
+        else:
+            bag.pop(item_id)
+            messages.success(request,
+                             (f'Removed {product.name} '
+                              f'from your bag'))
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
--- the_roastery/settings.py
+++ the_roastery/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '8000-grawnya-theroasteryecom-kgnd1cn50my.ws-eu99.gitpod.io']
+ALLOWED_HOSTS = ['localhost', '8000-grawnya-theroasteryecom-kgnd1cn50my.ws-eu100.gitpod.io']
 
 
 # Application definition
