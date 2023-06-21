a091064d1ede55b86ef5e57e32fef323b751016f
Grainne Donegan <grainnejdonegan@gmail.com>
2023-06-21 20:52:40 +0000

    update the admin panel with the order details

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class OrderItemAdminInline(admin.TabularInline):
+    model = OrderItem
+    readonly_fields = ('item_total_price',)
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderItemAdminInline,)
+
+    readonly_fields = ('order_id', 'order_date', 'items_total',
+                       'final_total', 'original_shopping_bag',
+                       'stripe_pid')
+
+    fields = ('order_id', 'order_date', 'full_name',
+              'email', 'phone_number', 'street_address1',
+              'street_address2', 'town_or_city', 'county', 
+              'postcode', 'country', 'items_total', 'delivery_cost', 
+              'final_total', 'original_shopping_bag', 'stripe_pid')
+
+    list_display = ('order_id', 'order_date', 'full_name',
+                    'items_total', 'delivery_cost',
+                    'final_total',)
+
+    ordering = ('-order_date',)
+
+admin.site.register(Order, OrderAdmin)
