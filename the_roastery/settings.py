4df93e7535485763764c811e0ee8e9e62650affd
Grainne Donegan <grainnejdonegan@gmail.com>
2023-06-22 19:40:06 +0000

    add crispy forms for bootstrap 5 requirement

--- the_roastery/settings.py
+++ the_roastery/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'crispy_forms',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -49,6 +50,10 @@ INSTALLED_APPS = [
     'shopping_bag',
     'checkout',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
