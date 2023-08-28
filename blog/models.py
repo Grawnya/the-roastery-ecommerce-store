420bcb7ea80aea1e8b60ec2a733922c024f0db79
Grainne Donegan <grainnejdonegan@gmail.com>
2023-08-28 19:10:51 +0000

    create a new blog entry view

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    heading = models.CharField(max_length=200)
+    blog_body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.heading}: {self.blog_body}'
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import *
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def view_blog(request):
-    return render(request, 'blog/blog.html')
+    entries = BlogEntry.objects.all()
+    context = {
+        'blogs': blogs
+    }
+    return render(request, 'blog/blog.html', context)
+
+@login_required
+def create_blog(request):
+    if request.method == 'POST':
+        blog_form = BlogForm(request.POST)
+        if blog_form.is_valid():
+            blog_form.save()
+            return redirect('blog')
+    else:
+        blog_form = BlogForm()
+
+    context = {
+        'form': blog_form
+    }
+    return render(request, 'blog_app/new_blog.html', context)
