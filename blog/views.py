a07919e3175d8458698df93aadb1ed0f049b2fe6
Grainne Donegan <grainnejdonegan@gmail.com>
2023-08-28 19:19:09 +0000

    create edit and delete blog functionality

--- blog/views.py
+++ blog/views.py
@@ -12,6 +12,11 @@ def view_blog(request):
 
 @login_required
 def create_blog(request):
+
+    if not request.user.is_superuser:
+        pass #error message
+        return redirect(reverse('blog'))
+
     if request.method == 'POST':
         blog_form = BlogForm(request.POST)
         if blog_form.is_valid():
@@ -24,3 +29,36 @@ def create_blog(request):
         'form': blog_form
     }
     return render(request, 'blog_app/new_blog.html', context)
+
+@login_required
+def edit_blog(request, blog_id):
+
+    if not request.user.is_superuser:
+        pass #error message
+        return redirect(reverse('blog'))
+
+    edit_blog = get_object_or_404(Blog, id=blog_id)
+    if request.method == 'POST':
+        blog_form = BlogForm(instance=edit_blog)
+        if blog_form.is_valid():
+            blog_form.save()
+            return redirect('blog')
+    else:
+        blog_form = BlogForm()
+
+    context = {
+        'form': blog_form
+    }
+    return render(request, 'blog_app/edit_blog.html', context)
+
+@login_required
+def delete_blog(request, blog_id):
+
+    if not request.user.is_superuser:
+        pass #error message
+        return redirect(reverse('blog'))
+
+    delete_blog = get_object_or_404(Blog, id=blog_id)
+    delete_blog.delete()
+    return redirect(reverse('blog'))
+    
